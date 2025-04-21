70396250bec2b762711c0a5b69fe7fca68638993
VinaPotenztech <vina@potenztech.com>
2025-04-21 15:48:45 +0530

    added integration testing

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,8 +1,9 @@
+// src/jest.config.js
 export default {
   testEnvironment: 'node',
-  transform: {},
-  extensionsToTreatAsEsm: ['.js'],
+  transform: {}, // No Babel, just pure ESM
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  testMatch: ['**/tests/**/*.test.js'],
 };
