9f57f5be59616ad627dd8c8bce9adb3cec63cccc
VinaPotenztech <vina@potenztech.com>
2025-04-22 10:09:53 +0530

    modifed testing flow

--- src/tests/unit/auth.routes.test.js
+++ src/tests/unit/auth.routes.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../../app.mjs';
+
 describe('Auth Routes', () => {
   describe('POST /employer-signup', () => {
     it('should register a user', async () => {
