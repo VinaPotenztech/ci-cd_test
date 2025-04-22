dc4ae0ffe069d73d88463ed1ef346919c68380f1
VinaPotenztech <vina@potenztech.com>
2025-04-22 10:25:20 +0530

    modifed testing flow

--- src/jest.config.mjs
+++ src/jest.config.mjs
@@ -6,9 +6,9 @@ export default {
   testEnvironment: 'node',
   transform: {},
   moduleFileExtensions: ['js', 'mjs', 'json'],
-  testMatch: ['**/tests/**/*.test.js'],
-  roots: ['<rootDir>/src'], // Update this path as necessary
-  moduleDirectories: ['node_modules', 'src'],
+  testMatch: ['<rootDir>/tests/**/*.test.js'], // now <rootDir> is src/
+  roots: ['<rootDir>'], // this will point to src/
+  moduleDirectories: ['node_modules', '<rootDir>'], // so imports work like 'routes/somefile'
   moduleNameMapper: {
     '^(.+).js$': '$1',
     'ipaddr.js': require.resolve('ipaddr.js'),
